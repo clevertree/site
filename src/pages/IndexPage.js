88600f8aa477b4239546f4f8525e9e1566c78412
Ari Asulin <ari.asulin@gmail.com>
2021-03-19 21:06:59 -0700

    css fixes

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -59,49 +59,63 @@ class IndexPage extends PageTemplate {
         <h1>Skills and Technologies</h1>
         <div className="experience skills">
           <h2>Programming</h2>
-          React Native,
-          ReactJS,
-          HTML5,
-          CSS3,
-          SQL,
-          Android Development,
-          iOS Development,
-          Javascript & jQuery,
-          PHP,
-          C#,
-          .NET,
-          Continuous Integration,
-          Encryption,
-          Application Programming Interfaces,
-          REST & GraphQL
+          <p>
+            React Native,
+            ReactJS,
+            HTML5,
+            CSS3,
+            SQL,
+            Android Development,
+            iOS Development,
+            Javascript & jQuery,
+            PHP,
+            C#,
+            .NET,
+            Continuous Integration,
+            Encryption,
+            Application Programming Interfaces,
+            REST & GraphQL
+          </p>
+
           <h2>Stack</h2>
-          NodeJS (Server) + React* (Client)
+          <p>
+            NodeJS (Server) + React* (Client),
+            LAMP (or nginx),
+          </p>
 
           <h2>Media</h2>
-          Video Editing,
-          Image Creation,
-          Music Production,
-          HTML & CSS Content
+          <p>
+            Video Editing,
+            Image Creation,
+            Music Production,
+            HTML & CSS Content
+          </p>
 
           <h2>Administration</h2>
-          Linux System Administration,
-          Nginx,
-          Apache,
-          SSL Certificates
-          Debian
+          <p>
+            Linux System Administration,
+            Nginx,
+            Apache,
+            SSL Certificates
+            Debian
+          </p>
 
           <h2>Tools</h2>
-          git,
-          ssh,
-          bash,
-          PHPStorm,
-          Visual Studio,
-          Gimp,
-          Kdenlive,
-          ObsStudio
+          <p>
+            git,
+            ssh,
+            bash,
+            PHPStorm,
+            Visual Studio,
+            Gimp,
+            Kdenlive,
+            ObsStudio
+          </p>
 
           <h2>Other</h2>
-          Juggling, Martial Arts, Investigation
+          <p>
+            Juggling, Martial Arts, Investigation
+          </p>
         </div>
       </section>
 
@@ -113,10 +127,10 @@ class IndexPage extends PageTemplate {
           <div className="duration">2002 - 2008</div>
           <h2 className="school">Arizona State University</h2>
           <div className="degree">B.S. in Computer Science</div>
-          <div className="description">
+          <p className="description">
             I taught martial arts at the Rec Center for over 3 years.
             Not the best school for learning programming.
-          </div>
+          </p>
         </div>
 
         <div className="experience education">
@@ -124,9 +138,9 @@ class IndexPage extends PageTemplate {
           <div className="duration">2001 - 2002</div>
           <h2 className="school">University of Arizona</h2>
           <div className="degree">B.S. in Computer Science</div>
-          <div className="description">
+          <p className="description">
             Nicer school than ASU with better programming courses.
-          </div>
+          </p>
         </div>
       </section>
 
@@ -138,14 +152,14 @@ class IndexPage extends PageTemplate {
           <div className="duration">2017 to present</div>
           <h2 className="company">Harkins Theatres</h2>
           <div className="position">Software Contractor</div>
-          <div className="description">
+          <p className="description">
             I was hired via odesk.com to maintain the Harkins.com website and application in PHP and ReactNative,
             which were languages the IT team had limited practice with.
             I helped them keep their software humming through hacker attacks, power outages, and all the rest.
             After the theatres closed due to the plandemic, my hours were reduced.
             As things aren't getting back to normal, I'm looking for new work.
             It was an amazing experience with amazing people ;D
-          </div>
+          </p>
           <div className="contact">
             <a href="mailto:DavePeep@harkins.com"><MdEmail /></a>
             <a href="tel:480 200-7231"><AiOutlinePhone /></a>
@@ -162,11 +176,11 @@ class IndexPage extends PageTemplate {
           <div className="duration">Aug 2016 – Dec 2018</div>
           <h2 className="company">Simon Payments</h2>
           <div className="position">Software Contractor</div>
-          <div className="description">
+          <p className="description">
             I rescued an East Coast payment processor who lost their programmer. I was able to fix their bank integration.
             Then I redesigned their entire payment processor in PHP.
             I integrated a few more bank APIs for Simon Payments and then eventually they let me go as the software was humming along beautifully.
-          </div>
+          </p>
 
           <div className="contact">
             <a href="mailto:tony@simonpayments.com"><MdEmail /></a>
@@ -179,11 +193,11 @@ class IndexPage extends PageTemplate {
           <div className="duration">Aug 2013 – Dec 2014</div>
           <h2 className="company">Originate and NewAer</h2>
           <div className="position">Originator and Software Engineer</div>
-          <div className="description">
+          <p className="description">
             I was an Originator for the Originate and NewAer software companies in San Francisco which operated out of the same offices.
             I was tasked with building Apps for Android, iOS, WinRT, Windows 8 desktop, as well as proximity­-based social networking.
             I then joined NewAer for the next 6 months and left after they realized Windows RT can't do bluetooth networking.
-          </div>
+          </p>
           <div className="contact">
             <a href="mailto:rob.mallery@originate.com"><MdEmail /></a>
             Rob Mallery
@@ -200,9 +214,9 @@ class IndexPage extends PageTemplate {
           <div className="duration">Feb 2012 – Apr 2013</div>
           <h2 className="company">Blue Global Media</h2>
           <div className="position">Integration Specialist, and System Administrator</div>
-          <div className="description">
+          <p className="description">
             I built APIs for lender applications and financial reporting as well as maintained linux servers.
-          </div>
+          </p>
           <div className="contact">
             <a href="mailto:eric@blueglobalmedia.com"><MdEmail /></a>
             Eric Cumberworth
